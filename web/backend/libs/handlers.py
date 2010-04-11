2ee3b87deb745573060e8593a8723272abc62f90
Eero af Heurlin <rambo@ragnaroek.(none)>
2010-04-11 19:20:54 +0300

    stub method for the exchange handler

--- web/backend/libs/handlers.py
+++ web/backend/libs/handlers.py
@@ -1,5 +1,6 @@
 import storage
 import tornado.web
+import time
 
 storage.instance.initialize()
 
@@ -15,7 +16,6 @@ class asyncwait(tornado.web.RequestHandler):
         return
 
     def wait_for_smth(self, callback, t=10):
-        import time
         if t:
             print "Sleeping 2 second, t=%s" % t
             tornado.ioloop.IOLoop.instance().add_timeout(time.time() + 2, lambda: self.wait_for_smth(callback, t-1))
@@ -27,3 +27,42 @@ class asyncwait(tornado.web.RequestHandler):
         self.write("Long running job complete")
         self.finish()
 
+class exchange(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def post(self):
+        # TODO: Use transaction
+        entry = storage.midgard.mgdschema.cardpunch_exchange()
+        stats_entry = storage.midgard.mgdschema.cardpunch_exchange_statistics()
+
+        stats_entry.fulllat = float(self.get_argument("lat"))
+        stats_entry.fulllon = float(self.get_argument("lon"))
+        stats_entry.geoaccuracy = self.get_argument("geoaccuracy")
+        stats_entry.geoprovider = self.get_argument("geoprovider")
+        stats_entry.devicemake = self.get_argument("devicemake")
+        stats_entry.devicemodel = self.get_argument("devicemodel")
+        stats_entry.devicefirmare = self.get_argument("devicefirmare")
+
+        stats_entry.create()
+        if self.stats_entry.id == 0:
+            # Bug http://trac.midgard-project.org/ticket/1409
+            raise Exception("Hit Midgard bug #1409 when creating stats_entry")
+
+        entry.servertime = int(time.time())
+        entry.timedelta = entry.servertime - int(self.get_argument("localtime"))
+        entry.vcard = self.get_argument("vcard")
+        entry.deviceid = self.get_argument("deviceid")
+        entry.lat = stats_entry.fulllat # TODO: round
+        entry.lon = stats_entry.fulllon # TODO: round
+        entry.initial = bool(self.get_argument("initial"))
+        entry.statistics = stats_entry.id
+
+        entry.create()
+        if self.entry.id == 0:
+            # Bug http://trac.midgard-project.org/ticket/1409
+            raise Exception("Hit Midgard bug #1409 when creating entry")
+
+        if entry.initial:
+            self.finish()
+
+        # TODO: Register async method to poll for the counterpart every second
+        self.finish()
