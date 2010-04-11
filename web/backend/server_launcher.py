8697d8f6c0398e7a75cd7b54670818f529438408
Eero af Heurlin <rambo@ragnaroek.(none)>
2010-04-11 18:48:36 +0300

    this way tornado sorta works

--- web/backend/server_launcher.py
+++ web/backend/server_launcher.py
@@ -6,8 +6,18 @@ local_module_dir = os.path.join(os.path.dirname(sys.argv[0]),  'libs')
 if os.path.isdir(local_module_dir):                                       
     sys.path.append(local_module_dir)
 
-from libs import server
+from libs import handlers
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+
+application = tornado.web.Application([
+    (r"/", handlers.MainHandler),
+])
 
 if __name__ == "__main__":
-    server.start()
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(8080)
+    tornado.ioloop.IOLoop.instance().start()
 
